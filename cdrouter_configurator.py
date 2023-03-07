48652fcd6a21783bfd18fa728050f9c0e7927a40
Lorcan Dillon Kelly <lorcandk@gmail.com>
2023-03-07 22:49:44 +0000

    user menu

--- cdrouter_configurator.py
+++ cdrouter_configurator.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 DUT_parameters = pd.read_csv('DUT_parameters.csv')
 
-print(DUT_parameters.to_string())
+# print(DUT_parameters.to_string())
 
 #input_options
 
@@ -11,53 +11,83 @@ input_options = {
     "DUT": {
         "parameter": "Device Under Test",
         "options": {"1": "DUT#1", "2": "DUT#2", "3": "DUT#3", "4": "DUT#4"},
-        "value": None
+        "input_value": "DUT#1"
     },
     "WAN Type": {
         "parameter": "WAN Type",
-        "options": {"1": "VDSL", "2": "GE-WAN/FTTH"},
-        "value": None
+        "options": {"1": "VDSL", "2": "GE-WAN"},
+        "input_value": "VDSL"
     },
     "WAN Protocol": {
         "parameter": "WAN Protocol",
         "options": {"1": "DHCP", "2": "PPPoE"},
-        "value": None
+        "input_value": "DHCP"
     },
     "Topology": {
         "parameter": "Topology",
-        "options": {"1": "Gateway Only", "2": "Mesh"},
-        "value": None
+        "options": {"1": "GATEWAY", "2": "MESH"},
+        "input_value": "GATEWAY"
     },
     "Reboot": {
-        "parameter": "Reboot or not",
-        "options": {"1": "Reboot", "2": "No-reboot"},
-        "value": None
+        "parameter": "Reboot",
+        "options": {"1": "REBOOT", "2": "NO-REBOOT"},
+        "input_value": "REBOOT"
     },
     "Users": {
         "parameter": "Users",
-        "options": {"1": "Single", "2": "Multi"},
-        "value": None
+        "options": {"1": "SINGLE", "2": "MULTI"},
+        "input_value": "SINGLE"
     },
     "IPv6": {
         "parameter": "IPv6",
         "options": {"1": "IPv6", "2": "IPv4"},
-        "value": None
+        "input_value": "IPv6"
     }
 }
 
-# Loop through the questions and ask the user to select their answer
+testvars = {
+        "wanInterface": "eth5",
+        "lanInterface": "eth2",
+        "lanSSID": "eir1234567"
+        }
 
-for question_number, input_options in input_options.items():
-    question_text = input_options["parameter"]
-    options = input_options["options"]
+for question_number, input_option in input_options.items():
+    question_text = input_option["parameter"]
+    options = input_option["options"]
+    default_value = input_option["input_value"]
     
     print(question_text)
     for option_number, option_text in options.items():
         print(f"{option_number}. {option_text}")
-    
-    selected_option = input("Enter the number of your answer: ")
-    input_options["value"] = options[selected_option]
+   
+    print("Default (1): " + default_value)
+    selected_option = input(f"Select number or Enter for default: ")
+    if len(selected_option) == 0 :
+        selected_option = "1"
+
+    print("Selected option " + str(selected_option) + " " + str(input_option["options"][selected_option]))
+    print()
+    input_option["input_value"] = input_option["options"][selected_option] 
 
 
-# Print the questions and answers
-print(input_options)
+# print parameter values
+for question_number, input_option in input_options.items():
+    print(f"{input_option['parameter']}: {input_option['input_value']}")
+print()
+
+
+WAN_Type = input_options["WAN Type"]["input_value"]
+
+if WAN_Type == "VDSL":
+    print(WAN_Type)
+    testvars["wanInterface"] = "eth5"
+    print(testvars["wanInterface"])
+
+elif WAN_Type == "GE-WAN":
+    print(WAN_Type)
+    testvars["wanInterface"] = "eth2"
+    print(testvars["wanInterface"])
+
+else:
+    print("ERROR")
+
