db08012fc60067979b4616765f4821c0772d7253
Lorcan Dillon Kelly <lorcandk@gmail.com>
2023-03-08 10:32:38 +0000

    testvars

--- cdrouter_configurator.py
+++ cdrouter_configurator.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
+# get the DUT parameters from the csv file
 DUT_parameters = pd.read_csv('DUT_parameters.csv')
+DUT_parameters_indexed = DUT_parameters.set_index('DUT')
 
-# print(DUT_parameters.to_string())
-
-#input_options
-
-# Define a dict to store the questions and options
+# Define a dict to store the options
 input_options = {
     "DUT": {
-        "parameter": "Device Under Test",
-        "options": {"1": "DUT#1", "2": "DUT#2", "3": "DUT#3", "4": "DUT#4"},
-        "input_value": "DUT#1"
+        "parameter": "DUT",
+        "options": {"1": "DUT1", "2": "DUT2", "3": "DUT3", "4": "DUT4"},
+        "input_value": "DUT1"
     },
     "WAN Type": {
         "parameter": "WAN Type",
@@ -45,12 +43,36 @@ input_options = {
     }
 }
 
+#define a dict for the testvars
 testvars = {
-        "wanInterface": "eth5",
-        "lanInterface": "eth2",
-        "lanSSID": "eir1234567"
+
+	"lan.lanClients":	"32",
+	"lan.lanInterface":	"GWMAC#1",
+	"lan2.lanBSSID":	"APMAC#1",
+	"lan2.lanChannel":	"5GHz",
+	"lan2.lanClients":	"32",
+	"lan2.lanInterface":	"wifi0-acn",
+	"lan2.lanSSID":		"SSID#1",
+    "lan2.wpaKey":		"WPA#1",
+    "lan3.lanBSSID":	"APMAC#1",
+    "lan3.lanChannel":	"2.4GHz",
+    "lan3.lanClients":	"1",
+    "lan3.lanInterface":	"wifi1-ax",
+    "lan3.lanSSID":		"3",
+    "lan3.wpaKey":		"WPA#2",
+    "pppoeUser":		"0",
+    "pppoePassword":	"0",
+    "RestartDut":		"/home/qacafe/powercycle.tcl 192.168.200.210 eth6 cyber cyber",
+    "RestartDutDelay":	"180",
+    "testvar_group.lan2":	"0",
+    "testvar_group.lan3":	"0",
+    "wanInterface":		"GWMAC#2",
+    "wanMode":	    	"DHCP",
+    "wanVlanId":		"1000"
+
         }
 
+#get user input
 for question_number, input_option in input_options.items():
     question_text = input_option["parameter"]
     options = input_option["options"]
@@ -76,6 +98,42 @@ for question_number, input_option in input_options.items():
 print()
 
 
+#set testvars for the DUT
+DUT = input_options["DUT"]["input_value"]
+
+column_DUT = DUT
+DUT_dict = DUT_parameters_indexed.loc[:, column_DUT].to_dict()
+
+testvars["RestartDut"] = "/home/qacafe/powercycle.tcl 192.168.200.210 " + DUT_dict["PDU"] + " cyber cyber"
+print(testvars["RestartDut"])
+
+testvars["lan.lanInterface"] = DUT_dict["LAN"]
+print(testvars["lan.lanInterface"])
+
+testvars["lan2.lanSSID"] = DUT_dict["SSID-5G"]
+print(testvars["lan2.lanSSID"])
+
+testvars["lan2.lanBSSID"] = DUT_dict["AP-MAC-5G"]
+print(testvars["lan2.lanBSSID"])
+
+testvars["lan2.wpaKey"] = DUT_dict["WPA"]
+print(testvars["lan2.wpaKey"])
+
+testvars["lan3.lanBSSID"] = DUT_dict["AP-MAC-2G"]
+print(testvars["lan3.lanBSSID"])
+
+testvars["lan3.lanSSID"] = DUT_dict["SSID-2G"]
+print(testvars["lan3.lanSSID"])
+
+testvars["lan3.wpaKey"] = DUT_dict["WPA"]
+print(testvars["lan3.wpaKey"])
+
+testvars["lan3.lanChannel"] = DUT_dict["WPA"]
+print(testvars["lan3.wpaKey"])
+
+
+
+# set testvars for the WAN Type
 WAN_Type = input_options["WAN Type"]["input_value"]
 
 if WAN_Type == "VDSL":
@@ -85,7 +143,8 @@ if WAN_Type == "VDSL":
 
 elif WAN_Type == "GE-WAN":
     print(WAN_Type)
-    testvars["wanInterface"] = "eth2"
+
+    testvars["wanInterface"] = DUT_dict["GE-WAN"]
     print(testvars["wanInterface"])
 
 else:
